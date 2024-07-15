2c8008a42d4e968e53d7765c9e84096fe4f32db1
juan-esteban-berger <juanestebanberger@gmail.com>
2024-07-14 23:02:40 -0600

    dnn python

--- python/dnn.py
+++ python/dnn.py
@@ -1,59 +1,65 @@
 import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
+import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from matplotlib import pyplot as plt
 
-######################################################################
+####################################################################
 # Load Data
-X_train = pd.read_csv('data/X_train.csv', header=None).values
-X_test = pd.read_csv('data/X_test.csv', header=None).values
-Y_train = pd.read_csv('data/Y_train.csv', header=None).values
-Y_test = pd.read_csv('data/Y_test.csv', header=None).values
+X_train = pd.read_csv('data/X_train.csv', header=None).to_numpy()
+X_test = pd.read_csv('data/X_test.csv', header=None).to_numpy()
+print(f"X_train: {X_train.shape}, X_test: {X_test.shape}")
 
-######################################################################
+Y_train = pd.read_csv('data/Y_train.csv', header=None).to_numpy()
+Y_test = pd.read_csv('data/Y_test.csv', header=None).to_numpy()
+print(f"Y_train: {Y_train.shape}, Y_test: {Y_test.shape}")
+
+####################################################################
+# Get Number of Training Examples
+m = X_train.shape[0]
+
+####################################################################
 # Transpose Data
 X_train = X_train.T
 X_test = X_test.T
+
 Y_train = Y_train.T
 Y_test = Y_test.T
 
-######################################################################
+####################################################################
+# Normalize X values
+X_test = X_test / 255.
+X_train = X_train / 255.
+
+####################################################################
 # Activation Function Classes
-class ReLu:
-    def __init__(self):
-        pass
-
+class Sigmoid():
     def function(self, Z):
-        return np.maximum(Z, 0)
+        return 1 / (1 + np.exp(-Z))
 
     def derivative(self, Z):
-        return Z > 0
+        return self.function(Z) * (1 - self.function(Z))
 
-class softmax:
-    def __init__(self):
-        pass
-
+class Softmax():
     def function(self, Z):
-        return np.exp(Z) /np.sum(np.exp(Z), axis=0)
+        e_Z = np.exp(Z - np.max(Z))
+        return e_Z / (e_Z.sum(axis=0) + 1e-8)
 
-    def derivative(self, Z):
-        S = self.function(Z)
-        return S * (1 - S)
+####################################################################
+# Loss Function Classes
+class CatCrossEntropy():
+    def function(self, Y, Y_hat):
+        return -np.sum(Y * np.log(Y_hat + 1e-8))
 
-######################################################################
-# Loss Functions
-### Need to add categorical cross entropy... (do not skip steps)
-    ### Categorical Cross Entropy eventually leads to A - Y...
-    ### Do it from scratch...
-
-######################################################################
+####################################################################
 # Layer Class
 class Layer:
     def __init__(self, input_num, output_num, activation):
         self.W = np.random.rand(output_num, input_num) - 0.5
-        self.b = np.zeros((output_num, 1)) - 0.5
+        self.b = np.random.rand(output_num, 1) - 0.5
        
         self.Z = None
         self.A = None
@@ -64,9 +70,9 @@ class Layer:
 
         self.activation = activation
 
-######################################################################
+####################################################################
 # Neural Network Class
-class NeuralNetwork:
+class NeuralNetwork():
     def __init__(self):
         self.layers = []
 
@@ -80,40 +86,78 @@ class NeuralNetwork:
             layer.A = layer.activation.function(layer.Z)
             A = layer.A
 
-    def backward(self, X, Y):
-        for i in reversed(range(0, len(self.layers))):
-            print(f"Iteration {i}")
+    def backward(self, X, Y, loss):
+        m = X.shape[1]
 
-    def update_params(self):
-        pass
+        for i in reversed(range(len(self.layers))):
+            layer = self.layers[i]
 
-    def train(self, X_train, Y_train, epochs, learning_rate, batch_size):
-        for epoch in range(0, epochs):
-            for i in tqdm(range(0, X_train.shape[1], batch_size),
-                          desc=f'Epoch {epoch+1}/{epochs}'):
-                X_batch = X_train[:, i:i+batch_size]
-                Y_batch = Y_train[:, i:i+batch_size]
+            if (i == len(self.layers) - 1 and
+               isinstance(layer.activation, Softmax) and
+               isinstance(loss, CatCrossEntropy)):
+                layer.dZ = layer.A - Y
+            else:
+                next_layer = self.layers[i + 1]
+                layer.dZ = (next_layer.W.T.dot(next_layer.dZ) *
+                            layer.activation.derivative(layer.Z))
 
-                self.forward(X_batch)
-                self.backward(X_batch, Y_batch)
-                self.update_params()
+            if i != 0:
+                prev_layer = self.layers[i - 1]
+                layer.dW = 1 / m * layer.dZ.dot(prev_layer.A.T)
+
+            else:
+                layer.dW = 1 / m * layer.dZ.dot(X.T)
+
+            layer.db = 1 / m * np.sum(layer.dZ, axis=1, keepdims=True)
+
+
+    def update_params(self, learning_rate):
+        for layer in self.layers:
+            layer.W -= learning_rate * layer.dW
+            layer.b -= learning_rate * layer.db
+
+    def get_accuracy(self, Y_train):
+        predictions = np.argmax(self.layers[-1].A, 0)
+        Y_decoded = np.argmax(Y_train, 0)
+        return np.sum(predictions == Y_decoded) / Y_decoded.size
+
+    def train(self, X_train, Y_train, epochs, learning_rate, loss):
+        pbar = tqdm(range(epochs), position=0, leave=True)
+        for epoch in pbar:
+            self.forward(X_train)
+            self.backward(X_train, Y_train, loss)
+            self.update_params(learning_rate)
+
+            acc = self.get_accuracy(Y_train)
+            loss_val = loss.function(Y_train, self.layers[-1].A)
+            description = ("Epoch: %d, Accuracy: %f, Loss: %.0f" %
+                           (epoch, acc, loss_val))
+            pbar.set_description(description)
 
     def predict(self):
         pass
 
-######################################################################
-# Initialize Neural Network
+    def save_config(self):
+        pass
+
+    def save_weights(self):
+        pass
+
+    def load_config(self):
+        pass
+
+    def load_weights(self):
+        pass
+
+####################################################################
 nn = NeuralNetwork()
 
-# Add Layers
-nn.add_layer(Layer(784, 10, ReLu()))
-nn.add_layer(Layer(10, 10, ReLu()))
-nn.add_layer(Layer(10, 10, softmax()))
+nn.add_layer(Layer(784, 200, Sigmoid()))
+nn.add_layer(Layer(200, 200, Sigmoid()))
+nn.add_layer(Layer(200, 10, Softmax()))
 
-# Train Neural Network
 nn.train(X_train,
          Y_train,
-         epochs=5,
-         # epochs=100,
-         learning_rate=0.01,
-         batch_size=200)
+         epochs=1000,
+         learning_rate=0.1,
+         loss=CatCrossEntropy())
